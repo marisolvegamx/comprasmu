806610ce21efc972fe592fc610af3c0762d03d7d
marisolvegamx <marisolvegamx@users.noreply.github.com>
2023-04-03 22:22:56 -0600

    empaque es por cliente

---
+++ app/src/main/java/com/example/comprasmu/EtiquetadoxCliente.java
@@ -0,0 +1,11 @@
+package com.example.comprasmu;
+
+public class EtiquetadoxCliente{
+    public int consCaja;
+    public int numMuestras;
+    public int numCaja;
+
+    public EtiquetadoxCliente() {
+
+    }
+}
