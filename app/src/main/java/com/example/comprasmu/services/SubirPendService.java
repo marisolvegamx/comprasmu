1efe2734fc5426d020151d7e19d4926a2956e0df
marisolvegamx <marisolvegamx@users.noreply.github.com>
2023-07-17 22:20:53 -0600

    ahora etiquetado y empaque es por ciudad cliente

--- app/src/main/java/com/example/comprasmu/services/SubirPendService.java
+++ app/src/main/java/com/example/comprasmu/services/SubirPendService.java
@@ -73,7 +73,7 @@ public class SubirPendService extends IntentService
 
 
                     //notificar();
-                    enviarReporte(objObservador);
+                  //  enviarReporte(objObservador);
                     // enviarOtra();
                 }catch (Exception ex){
 
