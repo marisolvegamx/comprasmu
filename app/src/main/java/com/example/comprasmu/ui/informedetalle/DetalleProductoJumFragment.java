de69a1fa435d752c945653a4338fb1ab26bde201
Marisol <marisolvegamx@hotmail.com>
2025-06-19 16:57:20 -0600

    poniendo registro en el log para encontrar errores de guardar informe

--- app/src/main/java/com/example/comprasmu/ui/informedetalle/DetalleProductoJumFragment.java
+++ app/src/main/java/com/example/comprasmu/ui/informedetalle/DetalleProductoJumFragment.java
@@ -19,6 +19,7 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.camera.lifecycle.ExperimentalCameraProviderConfiguration;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -487,6 +488,8 @@ public class DetalleProductoJumFragment extends DetalleProductoPenFragment{
                     //reviso si hay más clientes, si no fin
                     buscarClientes();
                     compraslog.grabarError(TAG+" finalizando buscando clientes ");
+                   if(2==2)
+                       throw new Exception("*****");
                     if (clientesAsig != null && clientesAsig.size() > 0) {
                         loadingDialog.dismisDialog();
                         yaestoyProcesando=false;
@@ -514,8 +517,8 @@ public class DetalleProductoJumFragment extends DetalleProductoPenFragment{
                     ex.printStackTrace();
                     loadingDialog.dismisDialog();
                     yaestoyProcesando=false;
-                    compraslog.grabarError(TAG+" HUBO UN ERROR AL FINALIZAR EL INFORME "+ex.getMessage());
-                    Toast.makeText(getActivity(), "HUBO UN ERROR AL FINALIZAR EL INFORME", Toast.LENGTH_LONG).show();
+                    compraslog.grabarError(TAG+" HUBO UN ERROR AL FINALIZAR EL INFORME, INTENTE ENVIAR DE NUEVO "+ex.getMessage());
+                    Toast.makeText(getActivity(), "HUBO UN ERROR AL FINALIZAR EL INFORME, INTENTE ENVIAR DE NUEVO", Toast.LENGTH_LONG).show();
                     aceptar.setEnabled(false);
 
                 }
