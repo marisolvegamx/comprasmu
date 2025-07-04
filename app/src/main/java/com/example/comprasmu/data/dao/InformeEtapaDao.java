3b145a7493db4ae5abc6c1dd61083ef164bff74d
Marisol <marisolvegamx@hotmail.com>
2025-07-04 13:04:31 -0600

    cambie mensaje cuando le dan click en envio

--- app/src/main/java/com/example/comprasmu/data/dao/InformeEtapaDao.java
+++ app/src/main/java/com/example/comprasmu/data/dao/InformeEtapaDao.java
@@ -136,7 +136,7 @@ public abstract class InformeEtapaDao extends  BaseDao<InformeEtapa>{
     @Query("SELECT * FROM informe_etapa WHERE   indice=:indiceSel and (estatus=:estatus) order by id desc")
     public abstract List<InformeEtapa> getInformesxEstatusAllSim(String indiceSel, int estatus);
 
-    @Query("SELECT * FROM informe_etapa WHERE   indice=:indice and (estatus=:estatus) and etapa=:etapa and ciudadNombre=:ciudad order by id desc")
+    @Query("SELECT * FROM informe_etapa WHERE   indice=:indice and (estatus=:estatus or estatus=7) and etapa=:etapa and ciudadNombre=:ciudad order by id desc")
     public abstract List<InformeEtapa>  getInfxEstatusCiuSim(String indice, int etapa, int estatus, String ciudad);
     /****muestro informes gastos abiertos o para agregar gastos nvo estatus 5****/
     @Query("SELECT * FROM informe_etapa WHERE  indice=:indice and (estatus=1 or estatus=5) and etapa=:etapa and ciudadNombre=:ciudad order by id desc")
