6f887e9c9bf9c4eaba1a74b56d2b21e333ccfb73
solecito <patitoeu@hotmail.com>
2025-07-29 16:02:37 -0600

    nueva tabla para codigos de muestras

---
+++ app/src/main/java/com/example/comprasmu/data/modelos/HistoricoMuestras.java
@@ -0,0 +1,23 @@
+package com.example.comprasmu.data.modelos;
+
+import androidx.room.Entity;
+
+@Entity(tableName = "historico_muestras")
+public class HistoricoMuestras {
+/*
+    private String inf_indice:
+    int inf_plantasid; plantaId,
+    n4_idn1 clienteId,
+            ind_informes_id,
+            ind_id,
+    ind_productos_id productoId,
+    pro_producto producto ,
+    ind_tamanio_id tamanioId,
+    cc.cad_descripcionesp as presentacion,
+    ind_empaque empaquesId,
+    cem.cad_descripcionesp empaque,
+    ind_caducidad caducidad,
+    ind_tipoanalisis tipoAnalisis,
+    cta.cad_descripcionesp as nombreAnalisis
+*/
+}
