af6d1e49d80714b102b9d043f87e3692b5019272
Marisol <marisolvegamx@hotmail.com>
2025-08-29 13:43:05 -0600

    valido wue para gastos no pueda reenviar el informe si esta en estatus en revision=7

--- app/src/main/java/com/example/comprasmu/data/dao/InformeEtapaDao.java
+++ app/src/main/java/com/example/comprasmu/data/dao/InformeEtapaDao.java
@@ -145,8 +145,8 @@ public abstract class InformeEtapaDao extends  BaseDao<InformeEtapa>{
     @Query("SELECT * FROM informe_etapa WHERE  indice=:indice and (estatus=1 or estatus=5) and etapa=:etapa order by id desc")
     public abstract List<InformeEtapa>  getInformePendGasSim(String indice, int etapa);
 
-    //gastos puede estar en 2 o 6
-    @Query("SELECT * FROM informe_etapa where etapa=:etapa and indice=:indice and (estatus=:estatus or estatus=6 )")
+    //gastos puede estar en 2 o 5,6o 7
+    @Query("SELECT * FROM informe_etapa where etapa=:etapa and indice=:indice and (estatus=:estatus or estatus>4 )")
     public abstract LiveData<List<InformeEtapa>> getInformesGastos(int etapa, String indice, int estatus);
 
     @Query("SELECT * FROM informe_etapa WHERE   indice=:indice and (estatus=:estatus or estatus=5 or estatus=6 or estatus=7) and etapa=:etapa and ciudadNombre=:ciudad order by id desc")
