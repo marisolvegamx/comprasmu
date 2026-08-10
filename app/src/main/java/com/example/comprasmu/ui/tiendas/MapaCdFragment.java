d3efb81edee2fa581d37fbec7c9ac92c4fc9e4f9
Marisol <marisolvegamx@hotmail.com>
2026-08-10 15:48:10 -0600

    ahora si pasé a una activity el mapa

--- app/src/main/java/com/example/comprasmu/ui/tiendas/MapaCdFragment.java
+++ app/src/main/java/com/example/comprasmu/ui/tiendas/MapaCdFragment.java
@@ -535,7 +535,7 @@ public class MapaCdFragment extends Fragment implements OnMapReadyCallback ,
                     Log.d(TAG, nav.getCurrentDestination().getId() + "--" + R.id.nav_tiendas);
                     if (nav.getCurrentDestination().getId() == R.id.nav_tiendas) {
 
-                        nav.navigate(R.id.action_buscartonuevo, bundle);
+                   //     nav.navigate(R.id.action_buscartonuevo, bundle);
                         //  NavHostFragment.findNavController(this).navigate(R.id.action_ciudadtohome);
                     }
                 }
@@ -547,7 +547,7 @@ public class MapaCdFragment extends Fragment implements OnMapReadyCallback ,
                 Log.d(TAG, nav.getCurrentDestination().getId() + "--" + R.id.nav_tiendas);
                 if (nav.getCurrentDestination().getId() == R.id.nav_tiendas) {
 
-                    nav.navigate(R.id.action_buscartonuevo, bundle);
+                  //  nav.navigate(R.id.action_buscartonuevo, bundle);
                     //  NavHostFragment.findNavController(this).navigate(R.id.action_ciudadtohome);
                 }
 
@@ -960,8 +960,8 @@ public class MapaCdFragment extends Fragment implements OnMapReadyCallback ,
         NavHostFragment navHostFragment =
                 (NavHostFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
-        if(navController!=null)
-            navController.navigate(R.id.action_buscartocdtrab);
+      //  if(navController!=null)
+      //      navController.navigate(R.id.action_buscartocdtrab);
 
     }
     private  void convertirLista(List<ListaCompra>lista){
@@ -1032,7 +1032,7 @@ public class MapaCdFragment extends Fragment implements OnMapReadyCallback ,
         this.doubleBackToExitPressedOnce = false;
         if(lastKnownLocation!=null) {
             bundle.putString("coordenasmapa", tienda.getUne_coordenadasxy() );  //ahora paso las coordenadas
-            NavHostFragment.findNavController(MapaCdFragment.this).navigate(R.id.action_buscartonuevo, bundle);
+          //  NavHostFragment.findNavController(MapaCdFragment.this).navigate(R.id.action_buscartonuevo, bundle);
             //return false;
         }else{
             Toast.makeText(getActivity(),"Espere para registrar su ubicación",Toast.LENGTH_LONG).show();
@@ -1112,7 +1112,7 @@ public class MapaCdFragment extends Fragment implements OnMapReadyCallback ,
         Log.d(TAG, nav.getCurrentDestination().getId() + "--" + R.id.nav_tiendas);
         if (nav.getCurrentDestination().getId() == R.id.nav_tiendas) {
 
-            nav.navigate(R.id.action_buscartonuevo, bundle);
+            //nav.navigate(R.id.action_buscartonuevo, bundle);
         }
     }
 
